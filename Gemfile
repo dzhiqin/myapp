e8193420308bc30f173e5e33b553fe4b7ccafadd
dzhiqin <dzhiqin@126.com>
2019-09-20 17:44:15 +0800

    markdown

--- Gemfile
+++ Gemfile
@@ -46,8 +46,8 @@ gem 'carrierwave'
 gem 'carrierwave-qiniu', '~> 1.1.7'
 gem 'slim-rails'
 
-# markdown
-gem 'redcarpet'
+# support markdown
+gem 'kramdown'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,7 @@
 module ApplicationHelper
-  def markdown(content)
-    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
-    options = {
-        autolink: true,
-        no_intra_emphasis: true,
-        disable_indented_code_blocks: true,
-        fenced_code_blocks: true,
-        lax_html_blocks: true,
-        strikethrough: true,
-        superscript: true
-    }
-    Redcarpet::Markdown.new(renderer, options).render(content).html_safe
-end
+  def kramdown(content)
+    require 'kramdown'
+
+    Kramdown::Document.new(content).to_html
+  end
 end
